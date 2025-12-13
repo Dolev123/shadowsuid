6ba4f35f1f966879af9b97452c4fe538eaf40cab
Dolev Niv <dolev.niv.2@gmail.com>
2025-12-14 00:44:37 +0200

    Update dummy to ssearch multiple paths

--- interpreter_dummy.c
+++ interpreter_dummy.c
@@ -1,10 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char * argv[], char * envp[])
 {
-    // please notice, on some OS's id is on /bin/
-    char * my_args[] = { "/usr/bin/id", NULL };
+    // check where id is
+    
+    FILE * f = NULL;
+    char* paths[] = {
+      "/bin/id",
+      "/usr/bin/id",
+    };
+    int i = 0;
+    for (;i < sizeof(paths); i++) {
+      if (!(f = fopen(paths[i], "r"))) {
+        if (errno == EACCES) {
+          printf("Failed with '%s'\n", paths[i]);
+          continue;
+        } else {
+          printf("Failed opening '%s' with error: %d, exiting.\n", paths[i], errno);
+          exit(errno);
+        }
+      }
+      break;
+    }
+    if (i == sizeof(paths)) {
+      printf("Failed to find a file, exiting.\n");
+      return -1;
+    }
+    
+    char * my_args[] = { paths[i] , NULL };
     setuid(0);
     setgid(0);
     execve(my_args[0], my_args, 0);
