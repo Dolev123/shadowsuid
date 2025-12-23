1ae8577e352772e6e42053e97e22e680ef89681c
Dolev123 <dolev.niv.2@gmail.com>
2025-12-23 02:16:36 +0200

    Refactor shadow_suid.py

--- shadow_suid.py
+++ shadow_suid.py
@@ -2,6 +2,12 @@
 import sys
 import os
 
+import argparse
+
+EPERM  = 1
+ENOENT = 2
+EEXIST = 17
+
 BINFMT_MISC_DIR = '/proc/sys/fs/binfmt_misc'
 
 def list_sahdow_suids():
@@ -12,48 +18,86 @@ def list_sahdow_suids():
             data = f.read()
             if 'C' in data.split('\n')[2]:
                 found_shadow_suids = True
-                print('Possible Shadow SUID rule: %s' % rule)
+                print(f"[+] Possible Shadow SUID rule: {rule}")
                 print('\t' + data.replace('\n', '\n\t'))
     if not found_shadow_suids:
-        print('Hooray! no possible Shadow SUIDs found!')
+        print('[+] Hooray! no possible Shadow SUIDs found!')
 
-def register_shadow_suid(rule_name, suid_path, interpreter_path):
+def register_shadow_suid(rule_name, suid_path, interpreter_path, magic_size=128):
+    format = lambda x: f"0{x}" if len(x) == 1 else x
     with open(suid_path, 'rb') as f:
-        hdr = "\\x"+"\\x".join(hex(x) for x in f.read(128))
-    with open(os.path.join(BINFMT_MISC_DIR, 'register'), 'wb') as f:
-        f.write(r":%s:M::%s::%s:C" % (rule_name, hdr, interpreter_path))
-
+        hdr = "\\x"+"\\x".join(format(hex(x)[2:]) for x in f.read(magic_size))
+    try:
+        register_path = os.path.join(BINFMT_MISC_DIR, 'register')
+        with open(register_path, 'wb') as f:
+            f.write(f":{rule_name}:M::{hdr}::{interpreter_path}:C".encode())
+        print("[+] shaddow suid installed")
+    except OSError:
+        print("[!] Failed to set shaddow binfmt_misc rule, probably tried to pass an invalid argument", file=os.sys.stderr)
+    except Exception as e:
+        print(f"[!] Failed to set shaddow binfmt_misc rule: {e}", file=os.sys.stderr)
 
 def unregister_shadow_suid(rule_name):
     with open(os.path.join(BINFMT_MISC_DIR, rule_name), 'wb') as f:
         f.write(b"-1")
+    print("[+] shaddow suid removed")
 
+def assert_root():
+    if os.geteuid() != 0:
+        print("[!] Not running as root, quitting...")
+        exit(EPERM)
+
+def assert_rule_exists(rule_name, should_exist: bool):
+    exists = os.path.exists(os.path.join(BINFMT_MISC_DIR, rule_name))
+    if should_exist and not exists:
+        print(f"[!] rule '{rule_name}' does not exist!", file=os.sys.stderr)
+        exit(ENOENT)
+    elif not should_exist and exists:
+        print(f"[!] rule '{rule_name}' already exists!", file=os.sys.stderr)
+        exit(EEXIST)
+
+def assert_suid_exsits(suid_path):
+    if not os.path.exists(suid_path):
+        print(f"[!] suid '{suid_path}' does not exist!", file=os.sys.stderr)
+        exit(ENOENT)
+
+def assert_interpreter_exsits(interpreter_path):
+    if not os.path.exists(interpreter_path):
+        print(f"[!] interpreter '{interpreter_path}' does not exist!", file=os.sys.stderr)
+        exit(ENOENT)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help="Requested action", dest="command")
+    list_parser = subparsers.add_parser("list", help="list all current shaddow SUIDs")
+    register_parser = subparsers.add_parser("register", help="register a new shaddow SUID")
+    register_parser.add_argument("-n", "--name", help="rule name", required=True)
+    register_parser.add_argument("-s", "--suid", help="suid path", required=True)
+    register_parser.add_argument("-i", "--interpreter", help="interpreter path", required=True)
+    unregister_parser = subparsers.add_parser("unregister", help="unregister an existing shaddow SUID")
+    unregister_parser.add_argument("-n", "--name", help="rule name", required=True)
+    args = parser.parse_args()
+    if args.command == None: 
+        parser.print_usage()
+        exit(0)
+    return args
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] == 'list':
+    args = parse_arguments()
+
+    if args.command == "list":
         list_sahdow_suids()
-
-    elif len(sys.argv) == 5 and sys.argv[1] == 'register':
-        _, _, rule_name, suid_path, interpreter_path = sys.argv
-        if os.path.exists(os.path.join(BINFMT_MISC_DIR, rule_name)):
-            print('Rule name "%s" already exists. failed.')
-            exit()
-        if not os.path.exists(suid_path):
-            print('"%s" does not exist. failed.' % suid_path)
-            exit()
-        if not os.path.exists(interpreter_path):
-            print('"%s" does not exist. failed.' % interpreter_path)
-            exit()
-        register_shadow_suid(rule_name, suid_path, interpreter_path)
-
-    elif len(sys.argv) == 3 and sys.argv[1] == 'unregister':
-        _, _, rule_name = sys.argv
-        if not os.path.exists(os.path.join(BINFMT_MISC_DIR, rule_name)):
-            print('Rule name "%s" does not exist. failed.')
-            exit()
-        unregister_shadow_suid(rule_name)
+    elif args.command == "register":
+        assert_root()
+        assert_rule_exists(args.name, False)
+        assert_suid_exsits(args.suid)
+        assert_interpreter_exsits(args.interpreter)
+        register_shadow_suid(args.name, args.suid, args.interpreter)
+    elif args.command == "unregister":
+        assert_root()
+        assert_rule_exists(args.name, True)
+        unregister_shadow_suid(args.name)
     else:
-        print('Usage:')
-        print('List all Shadow SUIDs:\n\t%s list' % sys.argv[0])
-        print('Register Shadow SUID:\n\t%s register <rule_name> <suid_path> <interpreter_path>' % sys.argv[0])
-        print('Unregister Shadow SUID:\n\t%s unregister <rule_name>' % sys.argv[0])
+        # should not get here, but in any case
+        raise TypeError(f"Invalid command: {args.command}")
+
